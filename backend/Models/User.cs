6b06445e7de54d73506b1dfbcd52b1b2b9c50c90
Sonumajuru <sonumajuru@gmail.com>
2025-11-22 16:23:18 +0100

    added missing props

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,18 +1,27 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace Payltr.Models;
-
-public class User
+namespace Payltr.Models
 {
-    [Key]
-    public Guid UserID { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Address { get; set; }
-    public string? City { get; set; }
-    public string? State { get; set; }
-    public string? Country { get; set; }
-    public bool IsDeleted { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public DateTime ModifiedAt { get; set; }
+    public class User
+    {
+        [Key]
+        public Guid UserID { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string PasswordSalt { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
 }
